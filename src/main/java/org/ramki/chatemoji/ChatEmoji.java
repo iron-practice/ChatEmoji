cd391b34115d51acbbe3d80a0fa3ab203a6168f4
ramenrrami <savio.tot@gmail.com>
2026-08-23 12:51:33 +0200

    add @Override to onEnabled & onDisabled

--- src/main/java/org/ramki/chatemoji/ChatEmoji.java
+++ src/main/java/org/ramki/chatemoji/ChatEmoji.java
@@ -6,12 +6,14 @@ import org.ramki.chatemoji.listeners.ChatListener;
 
 public class ChatEmoji extends JavaPlugin {
 
+    @Override
     public void onEnable() {
         saveDefaultConfig();
 
         Bukkit.getPluginManager().registerEvents(new ChatListener(this), this);
     }
 
+    @Override
     public void onDisable() {
 
     }
